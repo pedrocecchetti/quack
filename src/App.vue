1b8ebc55272a2e947873a9667be98eeff37d8e97
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-19 20:42:55 +0200

    feat: added Vuetify plugin

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,27 @@
 <template>
-  <div id="app">
+  <v-app>
     <NavBar />
-    <router-view />
-  </div>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue';
 export default {
-  components: {
-    NavBar
-  }
+  name: 'App',
+
+  components: { NavBar },
+
+  data: () => ({
+    //
+  })
 };
 </script>
 
 <style>
-body {
-  margin: 0;
-}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  margin: 0%;
-}
-
-#nav {
-  padding: 0;
-  width: 100%;
-  position: fixed;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  font-family: 'Baloo Bhaina 2', cursive;
 }
 </style>
