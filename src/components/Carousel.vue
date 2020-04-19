c4af61b17eecfebd499e083c1a974d75de97d04f
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-19 20:45:50 +0200

    feat: Created NavBar component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-carousel>
+      <v-carousel-item v-for="item in items" :key="item.id" :src="item.src">
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          src:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        },
+        {
+          id: 2,
+          src:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
