65e4e374416fb5a8d10fd5a4299a0e2ea1b39253
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-19 20:40:09 +0200

    feat: Created Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <v-app-bar class="navbar" app dark>
+    <div class="d-flex align-left">
+      <v-btn :to="{ name: 'Home' }" text x-large fab>Quack</v-btn>
+    </div>
+
+    <v-spacer></v-spacer>
+    <v-btn text :to="{ name: 'Home' }">Home</v-btn>
+    <v-btn disabled text>Posts Recentes </v-btn>
+    <v-btn text :to="{ name: 'Creators' }">
+      Criadores
+      <v-icon>mdi-account-multiple</v-icon>
+    </v-btn>
+    <v-btn href="https://youtube.com/c/quackbr" text target="_blank">
+      Nosso Canal
+      <v-icon>mdi-youtube</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
 export default {};
 </script>
-
-<style></style>
