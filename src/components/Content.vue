c2d7a81b0b88dea058566f435bc70ee7bcd08ea3
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-26 10:07:05 +0200

    feat: added content section to the design

---
+++ src/components/Content.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <Post v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<script>
+import Post from '@/components/Post.vue';
+export default {
+  components: { Post },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'Sejam Bem vindos a Nárnia',
+          content: 'Aqui jaz o texto da publicacao',
+          image:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        },
+        {
+          id: 1,
+          title: 'Sejam Bem vindos a Nárnia',
+          content: 'Aqui jaz o texto da publicacao',
+          image:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        },
+        {
+          id: 1,
+          title: 'Sejam Bem vindos a Nárnia',
+          content: 'Aqui jaz o texto da publicacao',
+          image:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        },
+        {
+          id: 1,
+          title: 'Sejam Bem vindos a Nárnia',
+          content: 'Aqui jaz o texto da publicacao',
+          image:
+            'https://images.unsplash.com/photo-1587058680684-e0a315822a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+v-sheet {
+  margin: 0;
+}
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="post">
+    <v-card-title outline>{{ post.title }} </v-card-title>
+    <v-img :src="post.image" alt="IMAGEM DO POST" />
+    <v-card-text>
+      {{ post.content }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.post {
+  margin-bottom: 3%;
+}
+</style>
