da628b017ff63a8516311631821ce7bfd0baff1f
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-27 23:31:32 +0200

    feat: Added navigation to the left side of the site

---
+++ src/components/PostsNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <ul class="uau">
+      <li v-for="route in routes" :key="route.path">
+        <v-btn v-if="route.component" text>{{ route.name }}</v-btn>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.uat {
+  margin: 0%;
+  padding: 0%;
+}
+
+ul {
+  list-style-type: none;
+  margin-bottom: 5%;
+  padding: 0%;
+  align-items: left;
+  text-align: left;
+}
+</style>
