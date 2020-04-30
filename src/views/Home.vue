19d5d783b87821e0df821e1f4372a39d61d7c10e
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-30 17:48:04 +0200

    feat: Added PostNav Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,11 @@
     <Carousel />
     <v-container>
       <v-row>
-        <v-col sm="12" md="2" :xs="2" fluid class="nav">
-          <h1>oi</h1>
+        <v-col sm="12" md="2" xs="2" fluid class="nav">
+          <PostsNav :routes="routes" />
         </v-col>
         <v-col sm="12" md="7" :xs="7" class="content">
-          <v-sheet color="#ffabcd">
+          <v-sheet color="#efefef">
             <Content />
           </v-sheet>
         </v-col>
@@ -26,11 +26,18 @@
 import CreatorsSidebar from '@/components/CreatorsSidebar.vue';
 import Carousel from '@/components/Carousel.vue';
 import Content from '@/components/Content.vue';
+import PostsNav from '@/components/PostsNav.vue';
 export default {
   components: {
     Carousel,
     CreatorsSidebar,
-    Content
+    Content,
+    PostsNav
+  },
+  data() {
+    return {
+      routes: this.$router.options.routes
+    };
   }
 };
 </script>
