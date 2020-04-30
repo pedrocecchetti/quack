30790d287b42584f14c9a13cf249bc55fb8aa931
Cecchetti <pedro.cecchetti@gmail.com>
2020-04-30 17:47:45 +0200

    feat: Changes the style of POSTNAV list

---
+++ src/components/PostsNav.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul id="link-list">
+      <li v-for="route in routes" :key="route.path">
+        <v-btn v-if="route.component" text>{{ route.name }}</v-btn>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+#link-list {
+  list-style-type: none;
+  margin-bottom: 5%;
+  padding-left: 0;
+  align-items: left;
+  text-align: left;
+}
+</style>
